a639109069dbd8fc0f1ad31127c8749212087eae
MichaelIsReallyAwesome <143132578+MichaelIsReallyAwesome@users.noreply.github.com>
2023-09-25 10:57:39 -0700

    Add files via upload

--- MotionGraphics.java
+++ MotionGraphics.java
@@ -6,26 +6,42 @@ import acm.graphics.GPoint;
 import acm.graphics.GPolygon;
 import acm.graphics.GRect;
 import acm.graphics.GRectangle;
+import java.awt.event.MouseEvent;
 
 /**
  * MotionGraphics.java
- * Paints an animated window with a square, circle and arrow
+ * Paints an animated window with an expanding square, 
+ * moving circle and moving arrow
  * @author Michael Yeung
  * @since September 21, 2023
  */
  
-public class MotionGraphics {
-	private GRectangle rect;
+public class MotionGraphics extends GraphicsProgram {
+	private GRect rect;
 	private GOval oval;
 	private GPolygon arrow;
+	private boolean movementPaused;
+	private int rectX, rectY, rectWidth, rectHeight;
 	
 	/** Initializes the fields in the program*/
 	public void init() {
-		
+		addMouseListeners();
+		rect=new GRect(300, 300, 80, 80);
+		add(rect);
+		movementPaused=false;
 	}
 	
 	/** Changes the size/location of shapes through timers */
 	public void run() {
-		
+		while (movementPaused) {
+			
+		}
+	}
+	/** Pauses shapes when mouse is clicked 
+	 * @param information on the mouse event that happened
+	 */
+	public void mouseClicked(MouseEvent e) {
+		if (movementPaused) movementPaused=false;
+		else movementPaused=true;
 	}
 }
